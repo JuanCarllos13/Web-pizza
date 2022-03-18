ccfbb29603f163ebc77923c145b38a68c6adbafb
juan <juan2017amaral@gmail.com>
2022-03-18 00:03:01 -0300

    Creating the home page input components

---
+++ src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import {Html, Head, Main, NextScript} from 'next/document'
+
+
+// Obrigatorio ser dessa 
+
+export default function Document(){
+    return(
+        <Html>
+            <Head>
+
+            </Head>
+            <body>
+                <Main/>
+                <NextScript/>
+            </body>
+        </Html>
+    )
+
+}
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,30 @@
+import Head from "next/head"
+import Image from "next/image"
+import LogoImg from '../../public/logo.svg'
+import styles from '../../styles/home.module.scss'
+import {Input} from '../components/ui/Input'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello word Netx.js </h1>
-    </div>
+    <>
+      <Head>
+        <title>SujeitoPizza - Faça seu login</title>
+      </Head>
+      <div className={styles.containerCenter}>
+        <Image src={LogoImg} alt="Logo do Pizzaria" />
+
+        <div className={styles.login}>
+          <form>
+            <Input placeholder="Digite seu Email" 
+            type={'text'}
+            />
+
+            <Input placeholder="Sua senha" type={'password'}/>
+
+          </form>
+        </div>
+
+      </div>
+    </>
   )
 }

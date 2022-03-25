879ad7660ec2e9bff00c296b4b3b922bb14c896c
juan <juan2017amaral@gmail.com>
2022-03-25 00:24:02 -0300

    Creating product page and category registration in the bank

---
+++ src/pages/product/index.tsx
@@ -0,0 +1,66 @@
+import Head from "next/head";
+import styles from './styles.module.scss'
+import { canSSRAuth } from '../../utils/canSSRAuth'
+import { Header } from "../../components/Header";
+
+export default function Product() {
+    return (
+        <>
+            <Head>
+                <title>Novo Produto</title>
+            </Head>
+            <div>
+                <Header />
+
+                <main className={styles.container}>
+                    <h1>Novo produto</h1>
+
+                    <form className={styles.form} >
+
+                        <select>
+                            <option>
+                                Bebida
+                            </option>
+                            <option>
+                                Pizza
+                            </option>
+                        </select>
+                        <input
+                            type={'text'}
+                            placeholder="Digite o nome do produto"
+                            className={styles.input}
+                        />
+
+
+                        <input
+                            type={'text'}
+                            placeholder="Preço do produto"
+                            className={styles.input}
+                        />
+
+                        <textarea
+                            placeholder="Descreva seu produto..."
+                            className={styles.input}
+                        />
+
+                        <button className={styles.button} type='submit'>
+                            Cadastrar
+                        </button>
+
+
+
+                    </form>
+                </main>
+            </div>
+        </>
+    )
+
+}
+
+
+
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+    return {
+        props: {}
+    }
+})
